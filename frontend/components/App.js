a791d5b06653ad233ee6a1336c4fe7cc2b3a5923
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-02-24 18:05:52 -0600

    Add state and Add check or uncheck task handler

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos:[
+
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+        
+      ]
+    }
+  }
   render() {
     return (
       <div>
         <h2>Todos:</h2>
 
         <ul>
-          <li>Walk the dog</li>
-          <li>Learn React</li>
-          <li>Have fun</li>
+          {
+            this.state.todos.map( todo => { return (<li>{todo.name} 
+            {todo.completed ? <span>  ✔  </span> : <span></span>  } </li>)})
+          }
+          
         </ul>
 
         <form>
