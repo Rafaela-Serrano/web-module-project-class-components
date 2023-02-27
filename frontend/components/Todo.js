8d670e7159de4d67b31c6e55cf5e9fe7aa5251fe
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-02-27 08:38:04 -0600

    use State, Props, Event Handlers

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default class Todo extends React.Component{
 
   handleClick = () => {
-    this.props.handleToggle(this.props.todo.id); 
+    this.props.handletoggled(this.props.todo.id); 
   }
 
   render(){
-    return( <li onClick = {this.handleClick} >{this.props.todo.name} {this.props.todo.completed ? <span>  ✔  </span> : <span></span>  } </li> ) ; 
+    return( <li onClick = {this.handleClick} > {this.props.todo.name} {this.props.todo.completed ? <span>  ✔  </span> : <span></span>  } </li> ) ; 
   }
 
 }
